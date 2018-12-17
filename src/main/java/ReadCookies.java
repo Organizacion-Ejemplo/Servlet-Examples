ebd3f7726e63416011247a59b18549d2f168b532
Adrian <amartinv1105@gmail.com>
2018-12-17 17:23:47 +0100

    commit test

--- src/main/java/ReadCookies.java
+++ src/main/java/ReadCookies.java
@@ -9,18 +9,7 @@ import javax.servlet.http.*;
  * @author Xavi Torrens
  */
 public class ReadCookies extends HttpServlet {
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response)
-            throws ServletException, IOException
-    {
-
-
-        List<CookieContainer> cookienames = new ArrayList<CookieContainer>();
-        // Get an array of Cookies associated with this domain
-        for (Cookie cookie : request.getCookies()){
-            CookieContainer cookieCt = new CookieContainer(cookie.getName(),cookie.getValue());
-
-            cookienames.add(cookieCt);
+    public void do
 
         }
 
