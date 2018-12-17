857e7b2107f44a88bbf685f9f7c5c7afb6752d21
Adrian <amartinv1105@gmail.com>
2018-12-17 17:14:56 +0100

    primer commit

--- src/main/java/CookieContainer.java
+++ src/main/java/CookieContainer.java
@@ -6,15 +6,7 @@ public class CookieContainer {
     private String name;
     private String value;
 
-    public CookieContainer(String name, String value) {
-        this.name = name;
-        this.value = value;
 
-    }
-
-    public String getValue() {
-        return value;
-    }
 
     public void setValue(String value) {
         this.value = value;
